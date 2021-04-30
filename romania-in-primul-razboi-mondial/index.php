6c6419b292de50965cf46b5f8fca61fd43496872
VladHilea <79455788+VladHilea@users.noreply.github.com>
2021-04-30 23:57:18 +0100

    Add files via upload

---
+++ romania-in-primul-razboi-mondial/index.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="ro-RO" dir="ltr">
+
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="user-scalable=yes, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/vegas.min.css">
+	<link rel="shortcut icon" href="images/favicon.ico" />
+	
+
+</head>
+<body>
+
+<!-- start menu transparent -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="pull-right navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand page-scroll opacity-60" href="#top">Romania in Primul Razboi Mondial</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      <ul class="nav navbar-nav navbar-right text-center ">
+        <li><a class="page-scroll" href="#about">Inainte de Razboi</a></li>
+        <li><a class="page-scroll" href="#program">1914-1916</a></li>
+        <li><a class="page-scroll" href="#prieteni">1916-1917</a></li>
+        <li><a class="page-scroll" href="#social">1917-1918</a></li>
+        
+        
+     
+    
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!-- sfarsit menu transparent --> 
+
+
+<section  class=" height-100" id="top">
+	<section class="opacity-black position-relative height-100">
+		<div class="container display-table height-100" >
+			<div class="row vertical-align" >
+				
+			</div>
+		</div>
+	</section>
+</section>
+ 
+
+
+</section>
+<section id="about" class="bg-black">
+<section class="bg-white">
+
+	<div class="container">
+		<div class="row text-center pb pt">
+			<h1><b>Inainte de Razboi</b></h1>
+		</div>
+	</div>
+</section>
+		<div class="container pb pt" >
+			<div class="row  text-center pb pt center-block font-white">
+				<div class="col-sm-6 ">
+  					<img class="img-responsive wow slideInUp " src="images/tripla-alianta.png" alt="...">
+
+   				</div>
+   				<div class="col-sm-6 ">
+  					<h4>La izbucnirea Primului Război Mondial, România era o țară aflată la finalul unei etape, de mai bine de 50 de ani, a unui proces de modernizare structurală. Acest proces a afectat toate domeniile vieții sociale, dar în mod diferit, astfel încât în primii ani ai secolului XX societatea românească cunoștea un nivel de dezvoltare diferit a segmentelor componente.
+            În ultimul deceniu dinaintea declanșării războiului au început să se manifeste tendințele unei încercări de reașezare a raporturilor României cu marile puteri europene. Din 1883, România era membră a Triplei Alianțe și, pe cale de consecință, în majoritatea situațiilor a urmat linia de conduită externă a acesteia, fără mari reușite în a reuși să o influențeze. Totuși, începând cu criza bosniacă și războaiele balcanice, România a început să se distanțeze de pozițiile Triplicei, în special de cele ale Austro-Ungariei. Are loc o reorientare în cadrul alianței tot mai mult spre Germania, ca partener preferat, concomitent cu dezvoltarea unui proces de consultare și coordonare a pozițiilor naționale cu Italia. </h4>
+
+   				</div>
+  					
+			
+		</div>
+</section>
+<section id="program">
+	<div class="container pt-50">
+		<div class="row text-center pt pb">
+			<div >
+				<h1 class="text-center text-font "><b>1914-1916</b></h1>	
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+<section class="bg-colo-1 ">
+
+
+	<div class="container pb pt" >
+			<div class="row  text-center pb pt center-block">
+				<div class="col-sm-6 ">
+  					<img class="img-responsive wow slideInUp " src="images/carol.jpg" alt="...">
+
+   				</div>
+   				<div class="col-sm-6 ">
+  					<h4>Din punct de vedere al statutului de beligeranță, România a fost țară neutră în perioada 13/28 iulie 1914 - 14/27 august 1916.</h4>
+            <h4>Imediat după izbucnirea ostilităților ambele tabere solicită României să intre în război de partea sa. Astfel, Germania, prin intermediul împăratului Wilhelm al II-lea și cancelarului T. von Bethmann-Hollweg, cereau regelui Carol I să pună în aplicare tratatul de alianță și să-și facă datoria de aliat. La rândul său, Antanta, avansa României prin intermediu ministrului plenipotențiar al Rusiei Stanislas Poklewski-Koziel propunere de intrare în acțiune de partea sa, în schimbul recunoașterii drepturilor asupra teritoriilor locuite de români din cadrul Imperiului Habsburgic.</h4>
+            <h4>Țara a întâmpinat criza departe de a fi unită. Existau dezacorduri politice serioase între rege și un mic grup de germanofili pe de o parte și majoritatea politicienilor si opinia publică, favorabile Antantei, pe de altă parte.</h4>
+            <h2> Dar ambele părți erau de acord că era imperativ necesar să se evite războiul.</h2>
+
+   				</div>
+   			</div>
+  					
+			
+		</div>
+</section>
+<section class="bg-black font-white">
+	<div class="container pt-50">
+		<div class="row text-center pt pb">
+			<div class="col-sm-6" style="color: lightgreen;">
+			
+				<h2> <b class="counter">51</b> <b ></b></h2>
+				<h3>Tari Participante</h3>
+			</div>
+
+			<div class="col-sm-6" style="color: red;">
+			
+				<h2><b class="counter">40000000 </b> <b> </b></h2>
+				<h3>Victime</h3>
+			</div>
+			
+		</div>
+	</div>			
+</section>
+</section>
+</section>
+
+<section id="prieteni">
+	<div class="container pb pt">
+		<div class="row  text-center pb">
+			<div class="col-sm-12 pb-60"> <h1><b>La 27 august 1916 România a declarat război <br>   Imperiului Austro-Ungar.</b></h1></div>
+				<div class="row  text-center pb pt center-block">
+				
+   				
+  					<h4><b>Operația ofensivă în Transilvania</b></h4>
+            <h5>Operația ofensivă strategică în Transilvania s-a desfășurat între 27 august și 26 septembrie 1916.
+
+Pentru desfășurarea operației, Marele Cartier General român destinase un număr de 3 armate (1, 2 și de Nord) cu un efectiv total de circa 420.000 de militari, reprezentând aproape 80% din efectivele Armatei de Operații. Obiectivul acestora era de „a înainta în Transilvania și Banat, cu scopul de a se concentra în vederea unei bătălii generale în zona Ciucea (spre nord) și Caransebeș (spre sud)”.</h5>
+            <h4><b>Operația de apărare pe frontul de sud</b></h4>
+            <h5>Planul de campanie prevedea ca pe frontul de sud Armata României să ducă o operație defensivă strategică. Inițial forțele Armatei 3 urmau să asigure acoperirea frontierei sudice de la Calafat la Marea Neagră urmând ca ulterior, după sosirea Corpului 47 Armată rus să desfășoare o operație ofensivă limitată în vederea scurtării liniei frontului pe linia Rusciuc - Șumla - Varna.Obiectivul acestor două operații era acela de a asigura libertatea de acțiune a forțelor principale ale Armatei României care operau în Transilvania.</h5>
+            <h4><b>Bătălia de la Turtucaia</b></h4>
+            <h5>Bătălia de la Turtucaia s-a desfășurat între 1-6 septembrie 1916. Pe 2 septembrie, forțele bulgaro-germane aflate sub comanda generalului Mackensen au început să atace pozițiile Armatei României din zona fortificată Turtucaia. </h5>
+            <h4><b>Bătălia de la Sibiu</b></h4>
+            <h5>Bătălia de la Sibiu s-a desfășurat între 26-29 septembrie 1916, în ea fiind angajate forțele Corpului 1 Armată român (Diviziile 13 și 23 infanterie și Brigada 1 cavalerie) și forțele Armatei 9 germane (Corpul 39 rezervă și Corpul Alpin). Corpul 1 Armată român ocupa un front de 55 de km la sud - sud-est de Sibiu, pe aliniamentul Racovița-Avrig-Săcădat-Cașolț-Gura Râului, cu flancurile sprijinite pe Munții Făgăraș la est și Munții Cibin la vest.</h5>
+            <h4><b>Bătălia de la Brașov</b></h4>
+            <h5>După victoria de la Sibiu, comandamentul Puterilor Centrale a declanșat contraofensiva împotriva dispozitivului Armatei 2 române care s-a retras prin luptă pe aliniamente succesive, pierzând tot teritoriu dintre Mureș și Olt cucerit în prima fază a operațiilor. Faza finală a acestei succesiuni de retrageri a fost bătălia de la Brașov, desfășurată între 7 și 8 octombrie 1916.</h5>
+            <h4><b>Bătălia de pe Valea Prahovei</b></h4>
+            <h5>ătălia pentru apărarea defileului Prahovei s-a desfășurat între 9-25 octombrie 1916 și a avut ca rezultat zădărnicirea încercării Armatei 9 germane de forțare a Munților Carpați pe „cel mai scurt drum spre București”. </h5>
+            <h4><b>Bătălia din zona Bran-Câmpulung</b></h4>
+            <h5>ătălia din zona Bran-Câmpulung s-a desfășurat între 8-23 octombrie 1916 și a avut ca rezultat anihilarea încercării Armatei 9 germane de forțare a Munților Carpați prin Culoarul Rucăr-Bran. </h5>
+            <h4><b>Bătălia pe Valea Oltului</b></h4>
+            <h5>Bătălia pentru apărarea defileului Oltului s-a desfășurat între 16 octombrie-26 noiembrie 1916 și a avut ca rezultat blocarea încercării Armatei 9 germane de a trece Carpații pe văile Oltului și Topologului.</h5>
+            <h4><b>Bătălia de pe Valea Jiului</b></h4>
+            <h5>Prima bătălie din defileul Jiului s-a desfășurat între 23 - 29 octombrie 1916. Obiectivul forțelor germano - austro-ungare era de a trece munții Carpați prin trecătorile Surduc (Lainici) și Vâlcan.</h5>
+            <h4><b>Operația de apărare a Bucureștiului</b></h4>
+            <h5>„Bătălia pentru București”, a avut ca obiectiv stoparea ofensivei forțelor Puterilor Centrale în Muntenia și prevenirea capturării capitalei țării, orașul București, de către acestea. Bătălia, desfășurată între 30 noiembrie și 3 decembrie 1916, a avut un caracter defensiv, la acțiunile militare participând alături de Armata României și un contingent din armata imperială rusă. Forțele atacatoare au fost preponderent germane, reprezentate de două grupări armate care atacau concentric, din Oltenia și din sudul Dunării, dar și de forțe bulgare și turce.</h5>
+
+
+
+   			
+   			</div>
+			</div>
+		</div>
+	</div>
+	</section>
+
+  <section class="bg-black">
+
+  <div class="container">
+    <div class="row text-center pb pt" style="color: white;">
+      <h1><b>1917-1918</b></h1>
+    </div>
+  </div>
+</section>
+	
+<section id="social">
+	<div class="container  ">
+		
+		<div class="row  text-center pb pt center-block">
+				<h4><b>Bătălia de la Mărăști</b></h4>
+            <h5>Bătălia de la Mărăști a fost prima din marile bătălii desfășurate pe teritoriul României în timpul campaniei anului 1917. S-a desfășurat între 22 iulie și 1 august 1917 fiind concepută ca o operație ofensivă pe o direcție secundară a armatelor române și ruse, în paralel cu operatia ofensivă principală de la Nămoloasa, la care s-a renunțat între timp. </h5>
+            <h5>Bătălia de la Mărăști s-a încheiat cu victoria forțelor române care au împins frontul înapoi pe o lățime de 30 de km și o adâncime de 20 de kilometri, eliberând peste 500 de kilometri pătrați și 30 de localități. Pierderile proprii au fost de 110 ofițeri și 4.782 de trupă (morți, răniți și dispăruți), iar cele provocate inamicului au constat în 32 ofițeri și 2.761 trupă prizonieri și o importantă cantitate de material de război.</h5>
+        <h4><b>A treia Bătălie de la Oituz</b></h4>
+            <h5>A treia Bătălie de la Oituz a avut loc între 26 iulie/8 august - 9/22 august 1917, concomitent cu Bătălia de la Mărășești. Acțiunile militare s-au desfășurat în două etape. Prima etapă a fost între 26 iulie/8 august - 5/18 august iar cea de-a doua etapă între 6/19—9/22 august, ambele etape reprezentând lupte duse pentru respingerea a două încercări ale forțelor Grupului Gerok, de a pătrunde în valea Trotușului. </h5>
+
+            <h2>Pe 27 martie 1918, Sfatul Țării din Basarabia a proclamat unirea cu România cu o majoritate a voturilor.Pe 14 octombrie, reprezentanții populației din Bucovina a votat pentru unirea cu Țara (unirea realizându-se pe 15 noiembrie), iar pe 1 decembrie același an, și reprezentanții românilor și sașilor din Transilvania a adoptat proclamația de la Alba Iulia de unire cu Regatul Român. </h2>
+
+   			</div>
+	</div>
+</section>
+
+
+<section class="bg-black">
+	<div class="container">
+		<div class="row text-center pb pt">
+			<div class="col-sm-12">
+				<h4>Created by Vlad Hilea</h4>
+				<h4>Copyright @ All rights reserved to Vlad Hilea</h4>
+				<h5>Vegas Slider by Jay Salvat</h5>
+			</div>
+		</div>
+	</div>
+</section>
+
+<a href="#top" id="goto" class='page-scroll dn  fs-18'>
+<span class="fa-stack fa-lg hidden-xs" style='position:fixed; bottom:60px; right:60px;'>
+  <i class="fa fa-circle fa-stack-2x"></i>
+  <i class="fa bg-yellow-1 img-circle fa-arrow-up font-white fa-stack-1x fa-inverse"></i>
+</span>
+</a>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+<script src="js/counterup.min.js"></script>
+ <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/typed.min.js"></script>
+<script src="js/vegas.min.js"></script>
+<script>  
+jQuery(window).scroll(function() {
+    if (jQuery(".navbar").offset().top > 50) {
+        jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+
+        } 
+    else
+        {
+        jQuery(".navbar-fixed-top").removeClass("nav-collapsed");
+
+        }
+});
+</script>
+<script>
+var width = $(window).width();
+if (width > 768) {jQuery(window).scroll(function() {
+    if (jQuery(".navbar").offset().top > 50) {
+        jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+
+        } 
+    else
+        {
+        jQuery(".navbar-fixed-top").removeClass("nav-collapsed");
+
+        }
+});
+}
+ else{
+ 	 jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+ }
+
+
+ 
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+     jQuery('.page-scroll').bind('click', function(e) {
+            e.preventDefault();
+            jQuery('html,body').animate({scrollTop: jQuery(this.hash).offset().top - 50});                                                         
+               });
+       $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 1250) {  
+       
+        $("#goto").removeClass("dn");
+    }
+    else{
+ $("#goto").addClass("dn");
+    }
+}); 
+</script>
+<script>
+    jQuery(document).ready(function( $ ) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+</script>
+<script >
+	$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+</script>
+<script >
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    center: true;
+    responsive:{
+        0:{
+            items:
+        }
+    }
+});
+</script>
+<script>
+	var typed = new Typed("#typed", {
+  strings: ["#RESPEEEECT ", "#PUTEREEEE ", "#MUIELACAI ", "#COLOSQUAD "],
+  typeSpeed: 50,
+  smartBackspace:true,
+    loop: true,
+  loopCount: Infinity,
+   backSpeed: 50,
+    backDelay: 800,
+
+});
+</script>
+<script>
+    $("#top").vegas({
+    slides: [
+        { src: "images/europa-fundal.jpg" },
+        { src: "images/romania-fundal.jpg" }
+    ],
+    animation: 'kenburns',
+    delay:8000
+});
+</script>
+</body>
+</html>
+
+
